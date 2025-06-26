f077f767c1eee65d1d4677648bebf13603fdaa5f
Rahul Chaudhary <rahulchaudhary2244@gmail.com>
2025-06-26 05:55:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+
+function App() {
+  return (
+    <>
+      <h1 className={clsx('text-3xl font-bold underline text-center')}>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
